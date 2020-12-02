5b65b83b4105fe97bd28616e0d68aae462ee9940
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2020-12-02 10:53:26 -0600

    1st commit

---
+++ Black_Out_Challenge/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Black_Out_Challenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetBlackOutDates(firstDateLabel, secondDateLabel);
+        }
+
+        private void SetBlackOutDates(Label firstDateLabel, Label secondDateLabel)
+        {
+            throw new NotImplementedException();
+        }
+
+        //first date
+        public void firstCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            firstDateLabel.Content = firstCalendar.SelectedDate.ToString();
+        }
+
+        //second date
+        public void secondCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            secondDateLabel.Content = secondCalendar.SelectedDate.ToString();
+        }
+
+    }
+}
